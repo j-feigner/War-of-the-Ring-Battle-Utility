3fddbe74a76efddde5dd08c5d7460ff73dd53fa0
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-02-01 17:00:25 -0800

    Added basic die roller
    Number of sides can be specified, but WotR uses D6s exclusively.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 window.onload = main;
 
 function main() {
-    
+    var dieRollerDisplay = document.querySelector(".dieRollerDisplay");
+    var dieRollerButton = document.querySelector(".dieRoller input");
+    dieRollerButton.addEventListener("click", () => {
+        dieRollerDisplay.innerHTML = rollDie(6);
+    })
+}
+
+function rollDie(sides) {
+    return Math.ceil(Math.random() * sides);
 }
