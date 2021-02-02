2a1fe3d6066d98e446764888695e9b65ac4f35a4
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-02-01 17:20:14 -0800

    Changed generic die roller to a WotR attack roller
    The new function calls rollDie() 5 times and returns the number of hits based on a given threshold (usually a result of 5+)

--- js/script.js
+++ js/script.js
@@ -4,8 +4,28 @@ function main() {
     var dieRollerDisplay = document.querySelector(".dieRollerDisplay");
     var dieRollerButton = document.querySelector(".dieRoller input");
     dieRollerButton.addEventListener("click", () => {
-        dieRollerDisplay.innerHTML = rollDie(6);
+        dieRollerDisplay.innerHTML = attack(5);
     })
+}
+
+function attack(hitThreshold) {
+    var rolls = [];
+    var hits = 0;
+
+    // Roll five six sided dice
+    for(var i = 0; i < 5; i++) {
+        var result = rollDie(6);
+        rolls.push(result);
+    }
+
+    // Determine hits in die results
+    rolls.forEach((value) => {
+        if(value >= hitThreshold) {
+            hits++;
+        }
+    }) 
+    
+    return hits;
 }
 
 function rollDie(sides) {
