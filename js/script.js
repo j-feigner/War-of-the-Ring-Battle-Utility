91100249c71907e73cd17790be72ecb09d96d362
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-02-08 12:13:10 -0800

    Code cleanup and commenting

--- js/script.js
+++ js/script.js
@@ -1,43 +1,19 @@
 window.onload = main;
 
 function main() {
-    var diceMaxNum = 5;
-
     var freePeoples = new Faction();
     var shadow = new Faction();
 
     freePeoples.unitInputs = document.querySelector("#battle-app #free-peoples.faction .armySettings .unitInputs");
+    freePeoples.statOutput = document.querySelector("#battle-app #free-peoples.faction .statWindow p");
+
     shadow.unitInputs = document.querySelector("#battle-app #shadow.faction .armySettings .unitInputs");
+    shadow.statOutput = document.querySelector("#battle-app #shadow.faction .statWindow p");
 
     var submitButton = document.querySelector("#battle-app #submit-button");
     submitButton.addEventListener("click", function() {
-        freePeoples.update();
-        shadow.update();
-
-        var freePeopleStats = "";
-        var shadowStats = "";
-
-        var freePeopleDist = hitDistribution(freePeoples.availableDice, freePeoples.leaders, 2 / 6);
-        var freePeopleMean = meanHits(freePeopleDist);
-        freePeopleDist.forEach((hitChance, hitValue) => {
-            var hitPercentage = hitChance * 100;
-            freePeopleStats += hitValue + " Hits: " + hitPercentage.toFixed(2) + "%\n";
-        })
-        freePeopleStats += "\n" + "Average Hits: " + freePeopleMean.toFixed(2) + "\n";
-
-        var shadowDist = hitDistribution(shadow.availableDice, shadow.leaders, 2 / 6);
-        var shadowMean = meanHits(shadowDist);
-        shadowDist.forEach((hitChance, hitValue) => {
-            var hitPercentage = hitChance * 100;
-            shadowStats += hitValue + " Hits: " + hitPercentage.toFixed(2) + "%\n";
-        })
-        shadowStats += "\n" + "Average Hits: " + shadowMean.toFixed(2) + "\n";
-
-        var freePeopleStatWindow = document.querySelector("#battle-app #free-peoples.faction .statWindow p");
-        var shadowStatWindow = document.querySelector("#battle-app #shadow.faction .statWindow p");
-
-        freePeopleStatWindow.innerHTML = freePeopleStats;
-        shadowStatWindow.innerHTML = shadowStats;
+        freePeoples.display();
+        shadow.display();
     })
 }
 
@@ -46,9 +22,10 @@ function Faction() {
     this.elites = 0;
     this.leaders = 0;
 
-    this.unitInputs = null;
-
     this.availableDice = 0;
+
+    this.unitInputs = null;
+    this.statOutput = null;
 
     this.update = function() {
         this.regulars = parseInt(this.unitInputs.querySelector(".regulars").value);
@@ -57,8 +34,25 @@ function Faction() {
 
         this.availableDice = Math.min(this.regulars + this.elites, 5);
     }
+
+    this.display = function() {
+        this.update();
+
+        var dist = hitDistribution(this.availableDice, this.leaders, 2 / 6);
+        var mean = meanHits(dist);
+
+        var output = "";
+        dist.forEach((hitChance, hitValue) => {
+            var hitPercentage = hitChance * 100;
+            output += hitValue + " Hits: " + hitPercentage.toFixed(2) + "%\n";
+        })
+        output += "\n" + "Average Hits: " + mean.toFixed(2) + "\n";
+
+        this.statOutput.innerHTML = output;
+    }
 }
 
+// Returns the mean for a discrete probability distribution array
 function meanHits(distribution) {
     var weightedTotal = 0;
     distribution.forEach((value, index) => {
@@ -67,6 +61,7 @@ function meanHits(distribution) {
     return weightedTotal;
 }
 
+// Creates a probability distribution of all possible hit values for a given number of dice, with rerolls.
 function hitDistribution(dice, rerolls, success) {
     var distribution = [];
     for(var i = 0; i <= dice; i++) {
@@ -75,39 +70,25 @@ function hitDistribution(dice, rerolls, success) {
     return distribution;
 }
 
+// Finds the probability of rolling a specific number of hits from a given dice pool, with rerolls.
 function hitProbability(hits, dice, rerolls, success) {
+    // Set success chance for first roll of dice pool
     var baseHitChance = success;
+
+    // Calculate success chance for a die given an allowed reroll on a miss
     var rerollHitChance = baseHitChance + ((1 - baseHitChance) * baseHitChance);
 
-    if(rerolls > dice) {
+    // Cap available rerolls to total number of available dice
+    if(rerolls > dice)
         rerolls = dice;
-    }
 
+    // Calculate total average hit chance for all dice
     var totalHitChance = (baseHitChance * (dice - rerolls) + rerollHitChance * rerolls) / dice;
 
     return binomialProbability(hits, dice, totalHitChance);
 }
 
-function attack(hitThreshold) {
-    var rolls = [];
-    var hits = 0;
-
-    // Roll five six sided dice
-    for(var i = 0; i < 5; i++) {
-        var result = rollDie(6);
-        rolls.push(result);
-    }
-
-    // Determine hits in die results
-    rolls.forEach((value) => {
-        if(value >= hitThreshold) {
-            hits++;
-        }
-    }) 
-
-    return hits;
-}
-
+// Returns a random number between 1 and sides. Simulates rolling an n-sided die.
 function rollDie(sides) {
     return Math.ceil(Math.random() * sides);
 }
