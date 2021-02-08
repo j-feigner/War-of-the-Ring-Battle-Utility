81dc9020f46a0ddb05d4b94c107e5383305d3110
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-02-07 19:42:36 -0800

    Added functions for calculating mean hits for a given probability distribution

--- js/script.js
+++ js/script.js
@@ -22,13 +22,11 @@ function main() {
         var freePeopleStats = "";
         var shadowStats = "";
 
-        for(var i = 0; i <= diceMaxNum; i++) {
-            var freePeoplesProbability = hitProbability(i, freePeopleDice, freePeopleLeaders);
-            var shadowProbability = hitProbability(i, shadowDice, shadowLeaders);
+        var freePeopleDist = hitDistribution(freePeopleDice, 2 / 6);
+        var freePeopleMean = meanHits(freePeopleDist);
 
-            freePeopleStats += i + " hits: " + freePeoplesProbability + "\n";
-            shadowStats += i + " hits: " + shadowProbability + "\n";
-        }
+        var shadowDist = hitDistribution(shadowDice, 2 / 6);
+        var shadowMean = meanHits(shadowDist);
 
         var freePeopleStatWindow = document.querySelector("#battle-app #free-peoples.faction .statWindow p");
         var shadowStatWindow = document.querySelector("#battle-app #shadow.faction .statWindow p");
@@ -38,8 +36,24 @@ function main() {
     })
 }
 
+function meanHits(distribution) {
+    var weightedTotal = 0;
+    distribution.forEach((value, index) => {
+        weightedTotal += value * index;
+    })
+    return weightedTotal;
+}
+
+function hitDistribution(dice, successChance) {
+    var distribution = [];
+    for(var i = 0; i <= dice; i++) {
+        distribution[i] = binomialProbability(i, dice, successChance);
+    }
+    return distribution;
+}
+
 function hitProbability(hits, dice, rerolls) {
-    
+    return binomialProbability(hits, dice, 2 / 6);
 }
 
 function attack(hitThreshold) {
