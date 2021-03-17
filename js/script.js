320623bbfabbaec1ff17da674a8a86952015a692
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-03-17 02:49:23 -0700

    Hit chance inputs now included in probability calculations, #3

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ function main() {
     var shadow = new Faction();
 
     freePeoples.unitInputs = document.querySelector("#battle-app #free-peoples.faction .armySettings .unitInputs");
+    freePeoples.hitInputs = document.querySelector("#battle-app #free-peoples.faction .hitSettings");
     freePeoples.statOutput = document.querySelector("#battle-app #free-peoples.faction .statWindow p");
 
     shadow.unitInputs = document.querySelector("#battle-app #shadow.faction .armySettings .unitInputs");
+    shadow.hitInputs = document.querySelector("#battle-app #shadow.faction .hitSettings");
     shadow.statOutput = document.querySelector("#battle-app #shadow.faction .statWindow p");
 
-    var inputs = document.querySelectorAll("#battle-app .faction .armySettings .unitInputs input");
+    var inputs = document.querySelectorAll("#battle-app .faction input");
     inputs.forEach((input) => {
         input.addEventListener("change", function() {
             freePeoples.display();
@@ -26,7 +28,11 @@ function Faction() {
 
     this.availableDice = 0;
 
+    this.baseHitChance = 0;
+    this.rerollHitChance = 0;
+
     this.unitInputs = null;
+    this.hitInputs = null;
     this.statOutput = null;
 
     this.update = function() {
@@ -35,12 +41,15 @@ function Faction() {
         this.leaders = parseInt(this.unitInputs.querySelector(".leaders").value);
 
         this.availableDice = Math.min(this.regulars + this.elites, 5);
+
+        this.baseHitChance = (6 - parseInt(this.hitInputs.querySelector(".baseroll").value) + 1) / 6;
+        this.rerollHitChance = (6 - parseInt(this.hitInputs.querySelector(".reroll").value) + 1) / 6;
     }
 
     this.display = function() {
         this.update();
 
-        var dist = hitDistribution(this.availableDice, this.leaders, 2 / 6);
+        var dist = hitDistribution(this.availableDice, this.leaders, this.baseHitChance, this.rerollHitChance);
         var mean = meanHits(dist);
 
         var output = "";
@@ -64,26 +73,26 @@ function meanHits(distribution) {
 }
 
 // Creates a probability distribution of all possible hit values for a given number of dice, with rerolls.
-function hitDistribution(dice, rerolls, success) {
+function hitDistribution(dice, rerolls, baseSuccess, rerollSuccess) {
     var distribution = [];
     for(var i = 0; i <= dice; i++) {
-        distribution[i] = hitProbability(i, dice, rerolls, success);
+        distribution[i] = hitProbability(i, dice, rerolls, baseSuccess, rerollSuccess);
     }
     return distribution;
 }
 
 // Finds the probability of rolling a specific number of hits from a given dice pool, with rerolls.
-function hitProbability(hits, dice, rerolls, success) {
+function hitProbability(hits, dice, rerolls, baseSuccess, rerollSuccess) {
     // If there are no rerolls available, take base probability
     if(rerolls == 0) {
-        return binomialProbability(hits, dice, success);
+        return binomialProbability(hits, dice, baseSuccess);
     } else {
         var total = 0;
 
         // Iterate for each possible hit value in the base roll
         for(var i = 0; i <= hits; i++) {
             // Chance of rolling i hits on base roll (0, 1, 2, ..., desired hits)
-            var baseRoll = binomialProbability(i, dice, success);
+            var baseRoll = binomialProbability(i, dice, baseSuccess);
 
             // Find dice remaining from rolling i hits
             var diceRemaining = dice - i;
@@ -92,7 +101,7 @@ function hitProbability(hits, dice, rerolls, success) {
             var rerollDice = Math.min(diceRemaining, rerolls);
 
             // Chance of rolling remaining hits with allocated reroll dice
-            var reroll = binomialProbability(hits - i, rerollDice, success);
+            var reroll = binomialProbability(hits - i, rerollDice, rerollSuccess);
 
             // Add current hit combination probability to total
             total += baseRoll * reroll;
@@ -100,9 +109,4 @@ function hitProbability(hits, dice, rerolls, success) {
 
         return total;
     }
-}
-
-// Returns a random number between 1 and sides. Simulates rolling an n-sided die.
-function rollDie(sides) {
-    return Math.ceil(Math.random() * sides);
 }
