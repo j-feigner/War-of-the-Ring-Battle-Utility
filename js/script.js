2814967cac9518243b5c3d6b20b5ba0184de1380
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-03-17 17:40:56 -0700

    Refactor and responsive app resizing added for mobile screens

--- js/script.js
+++ js/script.js
@@ -4,21 +4,8 @@ function main() {
     var freePeoples = new Faction();
     var shadow = new Faction();
 
-    freePeoples.unitInputs = document.querySelector("#battle-app #free-peoples.faction .armySettings .unitInputs");
-    freePeoples.hitInputs = document.querySelector("#battle-app #free-peoples.faction .hitSettings");
-    freePeoples.statOutput = document.querySelector("#battle-app #free-peoples.faction .statWindow");
-
-    shadow.unitInputs = document.querySelector("#battle-app #shadow.faction .armySettings .unitInputs");
-    shadow.hitInputs = document.querySelector("#battle-app #shadow.faction .hitSettings");
-    shadow.statOutput = document.querySelector("#battle-app #shadow.faction .statWindow");
-
-    var inputs = document.querySelectorAll("#battle-app .faction input");
-    inputs.forEach((input) => {
-        input.addEventListener("change", function() {
-            freePeoples.display();
-            shadow.display();
-        })
-    })
+    setFactionDOM(freePeoples, shadow);
+    setFactionEvents(freePeoples, shadow);
 
     freePeoples.display();
     shadow.display();
@@ -64,6 +51,26 @@ function Faction() {
         this.statOutput.querySelector(".hitDist").innerHTML = histDistOutput;
         this.statOutput.querySelector(".hitAvg").innerHTML = "Average Hits: " + mean.toFixed(2);
     }
+}
+
+function setFactionDOM(freePeoples, shadow) {
+    freePeoples.unitInputs = document.querySelector("#battle-app #free-peoples.faction .armySettings .unitInputs");
+    freePeoples.hitInputs = document.querySelector("#battle-app #free-peoples.faction .hitSettings");
+    freePeoples.statOutput = document.querySelector("#battle-app #free-peoples.faction .statWindow");
+
+    shadow.unitInputs = document.querySelector("#battle-app #shadow.faction .armySettings .unitInputs");
+    shadow.hitInputs = document.querySelector("#battle-app #shadow.faction .hitSettings");
+    shadow.statOutput = document.querySelector("#battle-app #shadow.faction .statWindow");
+}
+
+function setFactionEvents(freePeoples, shadow) {
+    var inputs = document.querySelectorAll("#battle-app .faction input");
+    inputs.forEach((input) => {
+        input.addEventListener("change", function() {
+            freePeoples.display();
+            shadow.display();
+        })
+    })
 }
 
 // Returns the mean for a discrete probability distribution array
